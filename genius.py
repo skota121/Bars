7d17eba2ec173c753fe732efcc061a26d1add15a
Sankruth Kota <sankruth.kota@gmail.com>
2017-07-31 14:36:34 -0400

    finished appending all songs now

--- genius.py
+++ genius.py
@@ -18,3 +18,21 @@ song_json = d.json()
 # Parse song_json for artist id
 artist_id = song_json["response"]["hits"][0]["result"]["primary_artist"]["id"]
 
+
+# Send another request to get songs written by an artist
+
+songs_url = 'https://api.genius.com/artists/{}/songs'.format(artist_id)
+
+songsbyartist = requests.get(url=songs_url, headers=headers)
+
+# Parse JSON again
+songs = songsbyartist.json()
+
+songs = songs["response"]["songs"]
+
+
+# Collect all the Song Titles and append to a list
+titles = []
+for song in songs:
+	titles.append(song["title"].encode('ascii'))
+print titles 
