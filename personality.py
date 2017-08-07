e6157ecf9164df114ae37bc89714a8bc48ba3af2
Sankruth Kota <sankruth.kota@gmail.com>
2017-08-06 21:43:23 -0400

    adding watson integration

--- personality.py
+++ personality.py
@@ -1,16 +1,22 @@
 import tweepy
 import json
+import sys
 consumer_key = ''
 consumer_secret = ''
 access_token = ''
 access_token_secret = ''
+personality_insights = PersonalityInsightsV3(version='2016-10-20', username='{username}', password='{password}')
+
+
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
+
 
 api = tweepy.API(auth)
 
 status = api.user_timeline(sys.argv[1], count=25)
 tweets = []
-
 for tweet in status:
-    print tweets.append(tweet._json["text"])
+	tweets.append(tweet._json["text"])
+print tweets[0]
