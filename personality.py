e2ceca9789fdefac38bdb68ba10c27d754d0ca03
Sankruth Kota <sankruth.kota@gmail.com>
2017-08-06 21:31:18 -0400

    getting all the statuses and appending them to a list

--- personality.py
+++ personality.py
@@ -1,10 +1,16 @@
 import tweepy
 import json
-consumer_key = 'secret'
-consumer_secret = 'secret'
-access_token = 'secret'
-access_token_secret = 'secret'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
+
+status = api.user_timeline(sys.argv[1], count=25)
+tweets = []
+
+for tweet in status:
+    print tweets.append(tweet._json["text"])
