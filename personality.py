b6f043c134a5b1e0833d3c2f6d658cd783140c84
Sankruth Kota <sankruth.kota@gmail.com>
2017-08-07 21:17:44 -0400

    realized neds arent part of this just wanna keep it to personalities and finished that

--- personality.py
+++ personality.py
@@ -19,19 +19,31 @@ auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 status = api.user_timeline(sys.argv[1], count=100)
+
 tweets = []
 for tweet in status:
 	tweets.append(tweet._json["text"])
-twit = ' '.join(tweets)
+tweeter = ' '.join(tweets)
+
 
 
 profile = personality_insights.profile(twit, content_type='text/plain', raw_scores=True, consumption_preferences=True)
-needs = profile["needs"]
+# needs = profile["needs"]
+personality = profile["personality"]
 
-insight_need = []
-insight_consume =[]
+# insight_need = []
+insight_personality = []
+raw_score_minimum = .80
+raw_score_personality = 0.60
+raw_score_trait = 0.70
 
-for need in needs:
-	insight_need.append(str(need["name"]))
+# for need in needs:
+# 	if need["raw_score"] > raw_score_minimum:
+# 		insight_need.append(str(need["name"])) 
 
-print  sys.argv[1],"'s",'main needs are:', insight_need[0],',', insight_need[1],',', insight_need[2]
+for personalities in personality:
+	if personalities["raw_score"] > raw_score_personality:
+		for personal in personalities["children"]:
+			if personal["raw_score"] > raw_score_trait:
+				insight_personality.append(str(personal["name"]))
+print insight_personality
