77c2bdb81cfc66f0feab87c48edac801acaedeae
Sankruth Kota <sankruth.kota@gmail.com>
2017-08-15 21:55:25 -0400

    set up some basic backend

--- bars/rappr/admin.py
+++ bars/rappr/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import rapperName
 
 # Register your models here.
+
+admin.site.register(rapperName)
--- bars/rappr/models.py
+++ bars/rappr/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class rapperName(models.Model):
+	rapper_name = models.CharField(max_length = 200)
+	def __str__(self):
+			return self.rapper_name
