5f1f453538ee5dbd924728e49e14f4c681c4f4c1
Sankruth Kota <sankruth.kota@gmail.com>
2017-08-16 22:30:08 -0400

    time to start on the templating

--- bars/bars/settings.py
+++ bars/bars/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '
+SECRET_KEY = '*+i57sqktles4om)5jrn22d%n)uhjaalsm*s@pz0d@l_z2z)*o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
